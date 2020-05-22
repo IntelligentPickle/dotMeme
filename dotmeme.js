e6e0da53750211b682d4dd407e4b13469720937b
Griffin Bauer <griffinrbauer@icloud.com>
2020-05-22 10:15:44 -0500

    added examples, edited template, added image creation time

--- dotmeme.js
+++ dotmeme.js
@@ -20,7 +20,7 @@ fs.readFile(filename, 'utf8', async function(err, data) {
     const out = fs.createWriteStream(__dirname + '/test.png')
     const stream = canvas.createPNGStream()
     stream.pipe(out)
-    out.on('finish', () =>  console.log('The PNG file was created.'))
+    out.on('finish', () =>  console.log(`Successfully assembled meme. (took ${process.uptime()} seconds)`))
 })
 
 
