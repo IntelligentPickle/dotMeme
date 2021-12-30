7fc86f61f9928d37fd9e9e10bd6f3d2f37539afe
Griffin Bauer <griffinrbauer@icloud.com>
2021-12-29 22:51:15 -0600

    add new logger preparing for render changes

--- dotmeme.js
+++ dotmeme.js
@@ -1,32 +1,33 @@
 const { createCanvas, loadImage } = require('canvas')
-var fs = require('fs'); // For reading files
+const fs = require('fs'); // For reading files
+const logger = require('./logger') // Set up default logger
+const winston = require('winston')
 
-
-filename = process.argv[2];
-fs.readFile(filename, 'utf8', async function(err, data) {
+if (!process.argv[2]) {
+    logger.error('Missing .meme argument.')
+    return;
+}
+fs.readFile(process.argv[2], 'utf8', async function(err, data) {
     if (err) {
-        console.error('Error reading .meme file:', err.message)
+        logger.error(`Error reading .meme file: \x1b[1m${err.message}\x1b[0m`)
         return;
     }; // There was an error reading the .meme file.
 
     // Parse the JSON.
     const parsedData = JSON.parse(data)
-
+    logger.info('Parsed JSON.')
 
     // TODO: make like everything better.
-    loadImage(parsedData.imageDetails.backgroundImage).then((image) => {
+    loadImage(parsedData.background).then((image) => {
     const canvas = createCanvas(image.width, image.height)
     const ctx = canvas.getContext('2d')
 
     ctx.drawImage(image, 0, 0, image.width, image.height)
 
-    ctx.font = parsedData.textDetails.topTextFont
-    ctx.fillText(parsedData.textDetails.topTextContent, 50, 100)
-
-    const out = fs.createWriteStream(__dirname + `out.png`)
-    const stream = canvas.createPNGStream()
-    stream.pipe(out)
-    out.on('finish', () =>  console.log(`Successfully assembled meme. (took ${process.uptime()} seconds)`))
+    // const out = fs.createWriteStream(__dirname + `out.png`)
+    // const stream = canvas.createPNGStream()
+    // stream.pipe(out)
+    // out.on('finish', () =>  console.log(`Successfully assembled meme. (took ${process.uptime()} seconds)`))
 })
 
 
