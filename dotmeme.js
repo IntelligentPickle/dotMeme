41d51eb1a3491efe25425efcadedcee57c66bfa5
Griffin Bauer <griffinrbauer@icloud.com>
2021-12-29 23:31:39 -0600

    check for missing elements, badly

--- dotmeme.js
+++ dotmeme.js
@@ -20,13 +20,20 @@ fs.readFile(process.argv[2], 'utf8', async function(err, data) {
     // TODO: make like everything better.
     loadImage(parsedData.background).then((image) => {
     logger.info(`Loaded background.`)
-    logger.info(`Canvas will be created with dimensions from bg: ${image.width}x${image.height}`)
+    logger.info(`Dimensions from bg: ${image.width}x${image.height}`)
     const canvas = createCanvas(image.width, image.height);
     const ctx = canvas.getContext('2d')
     ctx.drawImage(image, 0, 0, image.width, image.height)
     logger.info('Canvas created with background. Now writing text elements.')
 
-    parsedData.textElements.forEach(element => {
+    parsedData.textElements.forEach((element, i) => {
+        // Really bad checking :D
+        if (!element.font) { logger.warn(`Font is missing from element ${i}`)}
+        if (!element.color) { logger.warn(`Color is missing from element ${i}`)}
+        if (!element.text) { logger.warn(`Text is missing from element ${i}`)}
+        if (!element.x) { logger.warn(`X pos is missing from element ${i}`)}
+        if (!element.y) { logger.warn(`Y pos is missing from element ${i}`)}
+
         ctx.font = element.font
         ctx.fillStyle = element.color
         ctx.fillText(element.text, element.x, element.y)
@@ -35,7 +42,7 @@ fs.readFile(process.argv[2], 'utf8', async function(err, data) {
     const out = fs.createWriteStream(`outA.png`)
     const stream = canvas.createPNGStream()
     stream.pipe(out)
-    out.on('finish', () =>  console.log(`Successfully assembled meme. (took ${process.uptime()} seconds)`))
+    out.on('finish', () =>  logger.info(`Successfully assembled meme. (took ${process.uptime()} seconds)`))
 })
 
 
